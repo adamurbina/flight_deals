6d914d990d897e6ddaa3b0de4346937add661b9f
Adam <adamurbina@gmail.com>
2018-07-13 17:41:28 -0400

    Added url hash

--- lib/flight_deals/CLI.rb
+++ lib/flight_deals/CLI.rb
@@ -1,6 +1,8 @@
 class FlightDeals::CLI
+    attr_reader :CITIES
 
     CITIES = ["NYC", "SF", "LA", "BOSTON", "CHICAGO", "DALLAS", "MIAMI", "PHILLY", "PHOENIX", "PORTLAND", "SEATTLE", "DC"]
+    URL_LOOKUP = {"NYC" => "nyc", "SF" => "sfo", "LA" => "lax", "BOSTON" => "boston-flight-deals", "CHICAGO" => "chicago", "DALLAS" => "dallas", "MIAMI" => "miami", "PHILLY" => "philadelphia", "PHOENIX" => "phoenix", "PORTLAND" => "portland", "SEATTLE" => "seattle-flight-deals", "DC" => "dc"}
 
     def start
         puts "Welcome to the finding Daily Deals!"
@@ -10,17 +12,22 @@ class FlightDeals::CLI
 
     def display_cities
         CITIES.each_with_index do |city, index|
-            puts "#{index + 1}: #{city}"
+            puts "#{index + 1} >> #{city}"
         end
         puts "Enter city number to get deals:"
     end
 
     def get_deals
         input = gets.chomp
-        while input != "exit" && (1..CITIES.size).include?(input.to_i)
-            puts "interior reached"
+        if (1..CITIES.size).include?(input.to_i)
+            puts "url is #{URL_LOOKUP[CITIES[(input.to_i) - 1]]} "
+        elsif input == "exit"
+            puts "Goodbye see you next time!"
+            exit
+        else
+            puts "Input invalid. Please try again..."
+            get_deals
         end
-        puts "good bye"
     end
 
 
