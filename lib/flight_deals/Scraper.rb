31a6dad7703fe32690f59c0e6df9005e3da6d2ed
Adam <adamurbina@gmail.com>
2018-07-15 11:05:45 -0400

    Fixed deal scraper

--- lib/flight_deals/Scraper.rb
+++ lib/flight_deals/Scraper.rb
@@ -1,10 +1,29 @@
-class FlightDeals::CityDeals
+require "nokogiri"
+require "open-uri"
 
-    def scrape_city(city_name)
-        doc = Nokogiri::HTML(open("https://www.theflightdeal.com/category/flight-deals/dc/"))
-        titles = doc.css(".post-title").css("a").text
-        description = doc.css(".entry-content").css("p").text
-        puts description
+class Scraper
+
+    attr_accessor :deals
+
+    def self.city_scraper(city_name)
+        doc = Nokogiri::HTML(open("https://www.theflightdeal.com/category/flight-deals/#{city_name}/"))
+        deals = doc.css(".entry-content")
+        deals.collect do |deal|
+            new_hash = {}
+            new_hash[:title] = deal.css(".post-title").css("a").text
+            new_hash[:description] = deal.css("p").text
+            new_hash[:deal_url] = deal.css("a").attribute("href").value
+            new_hash
+        end
+        #titles = doc.css(".post-title").css("a").text
+        #description = deals.css("p").text
+
+    end
+
+    def self.deal_scraper(deal_url)
+
     end
 
 end
+
+puts Scraper.new.city_scraper("dc")
