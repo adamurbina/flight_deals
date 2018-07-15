d0421e8ea24a616fdec4a0126ed268d9e5a91cc0
Adam <adamurbina@gmail.com>
2018-07-15 14:28:22 -0400

    Added deal scraper

--- lib/flight_deals/CLI.rb
+++ lib/flight_deals/CLI.rb
@@ -5,22 +5,33 @@ class FlightDeals::CLI
     URL_LOOKUP = {"NEW YORK CITY" => "nyc", "SAN FRANCISCO" => "sfo", "LOS ANGELES" => "lax", "BOSTON" => "boston-flight-deals", "CHICAGO" => "chicago", "DALLAS" => "dallas", "MIAMI" => "miami", "PHILLY" => "philadelphia", "PHOENIX" => "phoenix", "PORTLAND" => "portland", "SEATTLE" => "seattle-flight-deals", "DC" => "dc"}
 
     def start
-        puts "Welcome to the finding Daily Deals!"
+        welcome
         display_cities
+        display_deals
         get_deals
     end
 
+    def welcome
+        puts "------------------------------------"
+        puts "     Welcome to Flight Deals!"
+        puts "------------------------------------"
+    end
+
     def display_cities
-        CITIES.each_with_index do |city, index|
-            puts "#{index + 1} >> #{city}"
+        CITIES.each.with_index(1) do |city, index|
+            puts "#{index} >> #{city}"
         end
         puts "Enter city number to get deals:"
     end
 
-    def get_deals
+    def display_deals
         input = gets.chomp
         if (1..CITIES.size).include?(input.to_i)
-            #puts "url is #{URL_LOOKUP[CITIES[(input.to_i) - 1]]} "
+            puts "Showing #{CITIES[(input.to_i)-1]} deals >>>"
+            deals = Scraper.city_scraper(URL_LOOKUP[CITIES[(input.to_i) - 1]])
+            deals.each.with_index(1) do |deal, i|
+                puts "#{i} >> #{deal[:title]}"
+            end
         elsif input == "exit"
             puts "Goodbye see you next time!"
             exit
@@ -30,6 +41,14 @@ class FlightDeals::CLI
         end
     end
 
-
+    def get_deals
+        puts "Select deal for more information or 'back' to go back."
+        input = gets.chomp
+        if input == "back"
+            start
+        elsif (1..CITIES.size).include?(input.to_i)
+            puts "REACHED!"
+        end
+    end
 
 end
--- lib/flight_deals/Scraper.rb
+++ lib/flight_deals/Scraper.rb
@@ -3,8 +3,6 @@ require "open-uri"
 
 class Scraper
 
-    attr_accessor :deals
-
     def self.city_scraper(city_name)
         doc = Nokogiri::HTML(open("https://www.theflightdeal.com/category/flight-deals/#{city_name}/"))
         deals = doc.css(".entry-content")
@@ -19,9 +17,12 @@ class Scraper
     end
 
     def self.deal_scraper(deal_url)
-
+        doc = Nokogiri::HTML(open(deal_url))
+        availability = doc.css(".entry-content").css("ul")[2].css("li").text
+        description = doc.css(".entry-content").css("a")[0].text
+        url = deal_url
     end
 
 end
 
-puts Scraper.new.city_scraper("dc")
+puts Scraper.deal_scraper("https://www.theflightdeal.com/2018/07/15/jetblue-134-boston-minneapolis-and-vice-versa-roundtrip-including-all-taxes/")
